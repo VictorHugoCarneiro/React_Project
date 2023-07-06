a94d89a3539203df8b7846a65414e1eb425070ff
Victor Carneiro <victorhugo.carneiro86@gmail.com>
2023-07-06 09:04:33 -0300

    Project REACT

---
+++ src/Hello.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+//function HelloWorld() = functions
+
+//class HelloWorld extends React.Component{
+//    render(){
+//        return <h1>Hello World</h1>
+//    }
+//} = class
+
+const HelloWorld = () => {
+ return <h1>Hello World</h1>
+ }
+
+  export default HelloWorld;
