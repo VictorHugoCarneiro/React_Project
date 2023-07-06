2f338bb03f1c56ae4c796823bab5ed9eec44c078
Victor Carneiro <victorhugo.carneiro86@gmail.com>
2023-07-06 12:10:43 -0300

    Props components

--- src/components/selecoes/selecao/index.js
+++ src/components/selecoes/selecao/index.js
@@ -1,12 +1,14 @@
 
 import React from 'react';
 import SelectImg from '../../shared/select_img';
+import DescriptionLink from '../../shared/description_link';
 
 const Selecao = (props) => {
     return (
         <div>
             <h4>{props.name}</h4>
-             <SelectImg img_url={props.img_url}/>
+            <DescriptionLink description={props.description} link={props.link}/>
+            <SelectImg img_url={props.img_url}/>
             <p>{props.description}</p>
            
         </div>
