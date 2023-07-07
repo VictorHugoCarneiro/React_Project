2464fe7c1331ec98736dff98c5f59bf4fe1ebaa1
Victor Carneiro <victorhugo.carneiro86@gmail.com>
2023-07-06 21:02:12 -0300

    Usando State e Lifecycle

--- src/components/selecoes/index.js
+++ src/components/selecoes/index.js
@@ -2,26 +2,27 @@
 import React, { Fragment } from "react";
 import Selecao from "./selecao";
 
+async function getSelecoes(){
+    let response = await fetch('http://localhost:3000/api/selecoes.json')
+    let data = await response.json()
+    return data;
+}
+
 class Selecoes extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            selecoes:[
-                {
-                    img_url:"https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Flag_of_Brazil.svg/150px-Flag_of_Brazil.svg.png",
-                    name:"Brasil",
-                    description:"Seleção Brasileira é o nome que refere à equipe que representa o Brasil em um determinado esporte. Para cada esporte, a Seleção Brasileira é formada pela seleção de jogadores profissionais e naturalizados brasileiros, geralmente a critério do técnico.",
-                    link:"https://pt.wikipedia.org/wiki/Sele%C3%A7%C3%A3o_Brasileira_de_Futebol",
-                },
-                {
-                    img_url:"https://upload.wikimedia.org/wikipedia/pt/thumb/f/fc/230px-Afa_logo.svg.png/120px-230px-Afa_logo.svg.png",
-                    name:"Argentina",
-                    description:"A Seleção Argentina de Futebol representa na Associação do Futebol Argentino nas competições de futebol da CONMEBOL e FIFA. Conhecida carinhosamente como La Albiceleste, a Argentina é uma das grandes seleções de futebol do mundo, tendo conquistado três Copas do Mundo em 1978, 1986 e 2022, quinze títulos da Copa América, uma Copa das Confederações em 1992, duas Copas Finalíssima em 1993 e 2022 e duas medalhas de ouro nos Jogos Olímpicos de 2004 e de 2008. Juntamente com o Brasil e com a França, a Argentina é uma das 3 seleções nacionais a ter conquistado as 3 mais importantes competições de futebol entre seleções:",
-                    link:"https://pt.wikipedia.org/wiki/Sele%C3%A7%C3%A3o_Argentina_de_Futebol",
-                }                
-            ]
+            selecoes: [ ]
         }
+    }
+
+    componentDidMount(){
+        getSelecoes().then(data => {
+            this.setState(state => ({
+                selecoes: data['selecoes']
+            }))
+        })
     }
 
     removeLast = () => {
@@ -46,12 +47,14 @@ class Selecoes extends React.Component {
                 <button onClick={this.removeLast}>Remove Last</button>
                 <button onClick={this.duplicateLastSelecao}>Duplicate Last</button>
                 <hr />
-                {this.state.selecoes.map((selecao) =>
+                {this.state.selecoes.map((selecao, index) =>
                 <Selecao 
+                    id={selecao.id}
                     img_url={selecao.img_url}
                     name={selecao.name}
                     description={selecao.description}
                     link={selecao.link}
+                    key={index}
                 />
                 )}
 
--- src/components/selecoes/selecao/index.js
+++ src/components/selecoes/selecao/index.js
@@ -3,21 +3,50 @@ import React from 'react';
 import SelectImg from '../../shared/select_img';
 import DescriptionLink from '../../shared/description_link';
 
-const Selecao = (props) => {
-    
-    let title;
-    if(props.title_whit_underline)
-        title = <h4><u>{props.name}</u></h4>
-    else
-        title = <h4>{props.name}</h4>    
-    return (
-        <div>
-            {title}
-            <DescriptionLink description={props.description} link={props.link}/>
-            <SelectImg img_url={props.img_url} gray={props.gray}/>
-           <hr/>
-        </div>
-    )
+async function getTimes(id){
+    let response = await fetch(`http://localhost:3000/api/${id}.json`)
+    let data = await response.json();
+    return data;
+}
+
+class Selecao extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            times: []
+        }
+    }
+
+    componentDidMount(){
+        getTimes(this.props.id).then(data => {
+            this.setState(state => ({
+                times: data['times']
+            }))
+        })
+    }
+
+    render(){
+        let title;
+        if(this.props.title_whit_underline)
+            title = <h4><u>{this.props.name}</u></h4>
+        else
+            title = <h4>{this.props.name}</h4> 
+        return (
+            
+            <div>
+                {title}
+                <DescriptionLink description={this.props.description} link={this.props.link}/>
+                <SelectImg img_url={this.props.img_url} gray={this.props.gray}/>
+                <h4>Times</h4>
+                <ul>
+                {this.state.times.map((time, index) => 
+                    <li key={index}>{time.name}</li>
+                )}
+                </ul>                
+               <hr/>
+            </div>
+        )
+    }
 }
 
 export default Selecao;
