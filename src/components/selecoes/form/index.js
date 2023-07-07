fb627a0f7e010f2575fa71a6d37f8c8aff9db77a
Victor Carneiro <victorhugo.carneiro86@gmail.com>
2023-07-07 16:12:49 -0300

    controlando multiplo inputs

--- src/components/selecoes/form/index.js
+++ src/components/selecoes/form/index.js
@@ -1,11 +1,24 @@
 import React, {Fragment, useState} from "react";
 
+const initialState = {
+    name: '',
+    description: '',
+    link: '',
+    img_url: ''
+
+}
+
 const Form = (props) => {
-    const [name, setName] = useState('')
-    const handleChange = Event => setName(Event.target.value);
+    const [fields, setFields] = useState(initialState)
+
+    const handleFieldsChange = (e) => setFields({
+        ...fields,
+        [e.currentTarget.name]: e.currentTarget.value
+    });
     const handleSubmit = Event => {
-        props.addSelecao({name: name})
+        props.addSelecao(fields)
         Event.preventDefault();
+        setFields(initialState);
     }
 
     return(
@@ -13,7 +26,19 @@ const Form = (props) => {
             <form onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Name: </label>
-                    <input id="name" type="text" value={name} onChange={handleChange}/>                    
+                    <input id="name" type="text" name="name" value={fields.name} onChange={handleFieldsChange}/>                    
+                </div>
+                <div>
+                    <label htmlFor="description">Description: </label>
+                    <input id="description" type="text" name="description" value={fields.description} onChange={handleFieldsChange}/>                    
+                </div>
+                <div>
+                    <label htmlFor="Link"> Link: </label>
+                    <input id="link" type="text" name="link" value={fields.link} onChange={handleFieldsChange}/>                    
+                </div>
+                <div>
+                    <label htmlFor="img_url"> Imagem: </label>
+                    <input id="img_url" type="img_url" name="img_url" value={fields.img_url} onChange={handleFieldsChange}/>                    
                 </div>
                 <br/>
                 <input type="submit"/>
